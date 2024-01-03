f36d80f465399729aff104ae2b88c80356f6e9bb
Hiroki <htani@figma.com>
2024-01-03 19:58:49 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+// import React from "react";
+import sensors from "./sensors.json";
+import { GeoJSON, MapContainer, TileLayer } from "react-leaflet";
+import { LatLng } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "./App.css";
+
+function App() {
+  const center = new LatLng(50.845100945010074, 14.741025136756022);
+
+  function onEachFeature(feature: any, layer: L.Layer) {
+    if (feature.properties) {
+      const { name, organization, url, thumbnailId } = feature.properties;
+      const thumbnail = thumbnailId
+        ? `<a href=${
+            `https://` + url
+          } target="_blank"><img src="/assets/${thumbnailId}.png" alt="${name} thumbnail image" class="dsg-popup-content-thumbnail" /></a>`
+        : "";
+      const content = `${thumbnail}<div class="dsg-popup-content-body"><h2 class="dsg-popup-content-name"><a href=${
+        `https://` + url
+      } target="_blank">${organization}</a></h2><p class="dsg-popup-content-organization">${name}</p><p class="dsg-popup-content-url">${url}</p></div>`;
+
+      layer.bindPopup(content);
+    }
+  }
+
+  return (
+    <div className="App">
+      <MapContainer center={center} zoom={2.4} minZoom={2.4} maxZoom={4}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <GeoJSON data={sensors} onEachFeature={onEachFeature} />
+      </MapContainer>
+    </div>
+  );
+}
+
+export default App;
+
+// https://platformland.github.io/government-design-systems/data/design-systems/
---
+++ src/thumbnail.ts
@@ -0,0 +1,1 @@
+export const digitalGoJp = "./assets/digitalGoJp.png";
